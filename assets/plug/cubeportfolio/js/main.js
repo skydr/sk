c5274bd204559237f132dda626a4bb639afcd639
Timonov Maksim <skydr@SkyBook.local>
2017-07-02 14:35:15 +0300

    zc

--- assets/plug/cubeportfolio/js/main.js
+++ assets/plug/cubeportfolio/js/main.js
@@ -1,137 +1,137 @@
 (function($, window, document, undefined) {
-    'use strict';
+  'use strict';
 
-    // init cubeportfolio
-    $('#js-grid-lightbox-gallery').cubeportfolio({
-        filters: '#js-filters-lightbox-gallery1, #js-filters-lightbox-gallery2',
-        layoutMode: 'grid',
-        mediaQueries: [{
-            width: 1500,
-            cols: 4,
-        }, {
-            width: 1100,
-            cols: 3,
-        }, {
-            width: 800,
-            cols: 3,
-        }, {
-            width: 480,
-            cols: 2,
-            options: {
-                caption: '',
-            }
-        }],
-        defaultFilter: '*',
-        animationType: 'flipOutDelay',
-        gapHorizontal: 10,
-        gapVertical: 10,
-        gridAdjustment: 'responsive',
-        caption: 'overlayBottomAlong',
-        displayType: 'sequentially',
-        displayTypeSpeed: 100,
+  // init cubeportfolio
+  $('#js-grid-lightbox-gallery').cubeportfolio({
+    filters: '#js-filters-lightbox-gallery1, #js-filters-lightbox-gallery2',
+    layoutMode: 'grid',
+    mediaQueries: [{
+      width: 1500,
+      cols: 4,
+    }, {
+      width: 1100,
+      cols: 3,
+    }, {
+      width: 800,
+      cols: 3,
+    }, {
+      width: 480,
+      cols: 2,
+      options: {
+        caption: '',
+      }
+    }],
+    defaultFilter: '*',
+    animationType: 'flipOutDelay',
+    gapHorizontal: 10,
+    gapVertical: 10,
+    gridAdjustment: 'responsive',
+    caption: 'overlayBottomAlong',
+    displayType: 'sequentially',
+    displayTypeSpeed: 100,
 
-        // lightbox
-        lightboxDelegate: '.cbp-lightbox',
-        lightboxGallery: true,
-        lightboxTitleSrc: 'data-title',
-        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+    // lightbox
+    lightboxDelegate: '.cbp-lightbox',
+    lightboxGallery: true,
+    lightboxTitleSrc: 'data-title',
+    lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
 
-        // singlePageInline
-        singlePageInlineDelegate: '.cbp-singlePageInline',
-        singlePageInlinePosition: 'below',
-        singlePageInlineInFocus: true,
-        singlePageInlineCallback: function(url, element) {
-            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
-            var t = this;
+    // singlePageInline
+    singlePageInlineDelegate: '.cbp-singlePageInline',
+    singlePageInlinePosition: 'below',
+    singlePageInlineInFocus: true,
+    singlePageInlineCallback: function(url, element) {
+      // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+      var t = this;
 
-            $.ajax({
-                    url: url,
-                    type: 'GET',
-                    dataType: 'html',
-                    timeout: 30000
-                })
-                .done(function(result) {
+      $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'html',
+          timeout: 30000
+        })
+        .done(function(result) {
 
-                    t.updateSinglePageInline(result);
+          t.updateSinglePageInline(result);
 
-                })
-                .fail(function() {
-                    t.updateSinglePageInline('AJAX Error! Please refresh the page!');
-                });
-        },
+        })
+        .fail(function() {
+          t.updateSinglePageInline('AJAX Error! Please refresh the page!');
+        });
+    },
 
-        plugins: {
-            loadMore: {
-                selector: '#js-loadMore-lightbox-gallery',
-                action: 'click',
-                loadItems: 3,
-            }
-        },
-    });
-    $('#js-grid-lightbox-gallery2').cubeportfolio({
-        filters: '#js-filters-lightbox-gallery133, #js-filters-lightbox-gallery233',
-        layoutMode: 'grid',
-        mediaQueries: [{
-            width: 1500,
-            cols: 4,
-        }, {
-            width: 1100,
-            cols: 3,
-        }, {
-            width: 800,
-            cols: 3,
-        }, {
-            width: 480,
-            cols: 2,
-            options: {
-                caption: '',
-            }
-        }],
-        defaultFilter: '*',
-        animationType: 'flipOutDelay',
-        gapHorizontal: 10,
-        gapVertical: 10,
-        gridAdjustment: 'responsive',
-        caption: 'overlayBottomAlong',
-        displayType: 'sequentially',
-        displayTypeSpeed: 100,
+    plugins: {
+      loadMore: {
+        selector: '#js-loadMore-lightbox-gallery',
+        action: 'click',
+        loadItems: 3,
+      }
+    },
+  });
+  $('#js-grid-lightbox-gallery2').cubeportfolio({
+    filters: '#js-filters-lightbox-gallery133, #js-filters-lightbox-gallery233',
+    layoutMode: 'grid',
+    mediaQueries: [{
+      width: 1500,
+      cols: 4,
+    }, {
+      width: 1100,
+      cols: 3,
+    }, {
+      width: 800,
+      cols: 3,
+    }, {
+      width: 480,
+      cols: 2,
+      options: {
+        caption: '',
+      }
+    }],
+    defaultFilter: '*',
+    animationType: 'flipOutDelay',
+    gapHorizontal: 10,
+    gapVertical: 10,
+    gridAdjustment: 'responsive',
+    caption: 'overlayBottomAlong',
+    displayType: 'sequentially',
+    displayTypeSpeed: 100,
 
-        // lightbox
-        lightboxDelegate: '.cbp-lightbox',
-        lightboxGallery: true,
-        lightboxTitleSrc: 'data-title',
-        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+    // lightbox
+    lightboxDelegate: '.cbp-lightbox',
+    lightboxGallery: true,
+    lightboxTitleSrc: 'data-title',
+    lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
 
-        // singlePageInline
-        singlePageInlineDelegate: '.cbp-singlePageInline',
-        singlePageInlinePosition: 'below',
-        singlePageInlineInFocus: true,
-        singlePageInlineCallback: function(url, element) {
-            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
-            var t = this;
+    // singlePageInline
+    singlePageInlineDelegate: '.cbp-singlePageInline',
+    singlePageInlinePosition: 'below',
+    singlePageInlineInFocus: true,
+    singlePageInlineCallback: function(url, element) {
+      // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+      var t = this;
 
-            $.ajax({
-                    url: url,
-                    type: 'GET',
-                    dataType: 'html',
-                    timeout: 30000
-                })
-                .done(function(result) {
+      $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'html',
+          timeout: 30000
+        })
+        .done(function(result) {
 
-                    t.updateSinglePageInline(result);
+          t.updateSinglePageInline(result);
 
-                })
-                .fail(function() {
-                    t.updateSinglePageInline('AJAX Error! Please refresh the page!');
-                });
-        },
+        })
+        .fail(function() {
+          t.updateSinglePageInline('AJAX Error! Please refresh the page!');
+        });
+    },
 
-        plugins: {
-            loadMore: {
-                selector: '#js-loadMore-lightbox-gallery',
-                action: 'click',
-                loadItems: 3,
-            }
-        },
-    });
+    plugins: {
+      loadMore: {
+        selector: '#js-loadMore-lightbox-gallery',
+        action: 'click',
+        loadItems: 3,
+      }
+    },
+  });
 })(jQuery, window, document);
