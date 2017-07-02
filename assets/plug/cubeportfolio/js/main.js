f828ad777ae90d9a93b415adc951362ba22913a6
Timonov Maksim <skydr@SkyBook.local>
2017-07-02 14:26:47 +0300

    up

--- assets/plug/cubeportfolio/js/main.js
+++ assets/plug/cubeportfolio/js/main.js
@@ -68,4 +68,70 @@
             }
         },
     });
+    $('#js-grid-lightbox-gallery2').cubeportfolio({
+        filters: '#js-filters-lightbox-gallery133, #js-filters-lightbox-gallery233',
+        layoutMode: 'grid',
+        mediaQueries: [{
+            width: 1500,
+            cols: 4,
+        }, {
+            width: 1100,
+            cols: 3,
+        }, {
+            width: 800,
+            cols: 3,
+        }, {
+            width: 480,
+            cols: 2,
+            options: {
+                caption: '',
+            }
+        }],
+        defaultFilter: '*',
+        animationType: 'flipOutDelay',
+        gapHorizontal: 10,
+        gapVertical: 10,
+        gridAdjustment: 'responsive',
+        caption: 'overlayBottomAlong',
+        displayType: 'sequentially',
+        displayTypeSpeed: 100,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePageInline
+        singlePageInlineDelegate: '.cbp-singlePageInline',
+        singlePageInlinePosition: 'below',
+        singlePageInlineInFocus: true,
+        singlePageInlineCallback: function(url, element) {
+            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 30000
+                })
+                .done(function(result) {
+
+                    t.updateSinglePageInline(result);
+
+                })
+                .fail(function() {
+                    t.updateSinglePageInline('AJAX Error! Please refresh the page!');
+                });
+        },
+
+        plugins: {
+            loadMore: {
+                selector: '#js-loadMore-lightbox-gallery',
+                action: 'click',
+                loadItems: 3,
+            }
+        },
+    });
 })(jQuery, window, document);
